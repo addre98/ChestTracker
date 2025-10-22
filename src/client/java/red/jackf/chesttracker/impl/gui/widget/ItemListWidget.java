7ba59c412fc9e404a9b3b1b7f0c2cdc271f8d1ce
ponuing <nikkor8325@gmail.com>
2025-10-22 16:30:57 +0300

    try fix

--- src/client/java/red/jackf/chesttracker/impl/gui/widget/ItemListWidget.java
+++ src/client/java/red/jackf/chesttracker/impl/gui/widget/ItemListWidget.java
@@ -15,7 +15,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 import red.jackf.chesttracker.impl.config.ChestTrackerConfig;
 import red.jackf.chesttracker.impl.gui.GuiConstants;
@@ -31,8 +30,6 @@ import java.util.stream.Collectors;
 
 public class ItemListWidget extends AbstractWidget {
     private static final ResourceLocation BACKGROUND_SPRITE = GuiUtil.sprite("widgets/slot_background");
-    private static final ItemStack DUMMY_ITEM_FOR_COUNT = new ItemStack(Items.EMERALD);
-
     private final int gridWidth;
     private final int gridHeight;
     private List<ItemStack> items = Collections.emptyList();
@@ -140,11 +137,16 @@ public class ItemListWidget extends AbstractWidget {
         if (!this.hideTooltip) {
             var stack = items.get(index);
             var lines = Screen.getTooltipFromItem(Minecraft.getInstance(), stack);
+            var font = Minecraft.getInstance().font;
             if (stack.getCount() > 999) {
                 lines.add(Component.literal(Strings.commaSeparated(stack.getCount()))
                         .withStyle(ChatFormatting.GREEN));
             }
             var image = stack.getTooltipImage();
+
+            if (font == null) {
+                return;
+            }
 
             List<ClientTooltipComponent> components = lines.stream()
                     .map(Component::getVisualOrderText)
@@ -154,7 +156,7 @@ public class ItemListWidget extends AbstractWidget {
             image.ifPresent(img -> components.add(0, ClientTooltipComponent.create(img)));
 
             graphics.renderTooltip(
-                    Minecraft.getInstance().font,
+                    font,
                     components,
                     mouseX,
                     mouseY + 12,
