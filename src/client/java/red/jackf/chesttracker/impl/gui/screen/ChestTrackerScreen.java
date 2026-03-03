649d8db5e0a0c8c50bc0d028245e0114dbc1a681
ponuing <nikkor8325@gmail.com>
2026-03-03 14:42:37 +0300

    [fix] Fix z-level tooltip render in ItemListWidget

--- src/client/java/red/jackf/chesttracker/impl/gui/screen/ChestTrackerScreen.java
+++ src/client/java/red/jackf/chesttracker/impl/gui/screen/ChestTrackerScreen.java
@@ -360,6 +360,7 @@ public class ChestTrackerScreen extends Screen {
         this.itemList.setHideTooltip(this.search.isFocused() && ifSearchables(a -> a.isMouseOver(mouseX, mouseY)));
         super.render(graphics, mouseX, mouseY, tickDelta); // widgets
         graphics.drawString(this.font, this.title, left + TITLE_LEFT, top + TITLE_TOP, TextColours.getLabelColour(), false); // title
+        this.itemList.renderTooltip(graphics);
     }
 
     @Override
